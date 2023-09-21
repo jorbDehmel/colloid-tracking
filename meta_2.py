0e84f4cf091d5572ad16cea1b3c28667f81e77dc
Jorb Dehmel <jdehmel@outlook.com>
2023-09-21 15:07:23 -0600

    Made meta2 output csv

--- meta_2.py
+++ meta_2.py
@@ -10,24 +10,37 @@ import reverser
 '''
 An analysis wherein all data from a given voltage is
 on the same graph.
-
-TODO:
-- Put all data from a given voltage on one graph
-
-For instance:
-
-For 5v:
-    - collect all 5v files which do not have STD in title
-    - graph each on the same graph by frequency
+'''
 
 '''
+Output table structure:
+
+rows are frequency applied
+
+columns are [voltage, solution, particle size, mean straight line speed, mean straight line speed std]
+'''
+
+# Relevant columns
+columns: [str] = ['VOLTAGE', 'SOLUTION',
+                  'SIZE_MICRONS', 'FREQUENCY_HZ', 'STRAIGHT_LINE_SPEED_PIXELS',
+                  'STRAIGHT_LINE_SPEED_STD']
+
+# Frequencies measured
+frequencies: [str] = ['0.0', '800.0', '1000.0', '10000.0', '25000.0',
+                      '50000.0', '75000.0', '100000.0', '150000.0', '200000.0', '300000.0']
+
+# Where to save the .csv file
+save_location: str = '/home/jorb/Programs/physicsScripts/output.csv'
+
+# Directory to work in
+dir: str = '/home/jorb/data_graphs/'
 
 
 # Returns ([data], [std_data])
 def load_voltage(pattern: str, save_path: str,
                  secondary_save_path: str, voltage: float,
                  do_graphs: bool = True,
-                 transitions: [str] = ['10000.0', '10000.0', '10000.0']) -> ([df.DataFrame], [df.DataFrame]):
+                 transitions: [str] = ['10000.0', '10000.0', '10000.0']) -> ([df.DataFrame], [df.DataFrame], [str]):
     # Get candidates
     candidates: [str] = name_fixer.find_all(pattern)
     candidates = [c for c in candidates if 'stds' not in c]
@@ -116,15 +129,58 @@ def load_voltage(pattern: str, save_path: str,
             plt.savefig(save_path + '.png')
 
         if secondary_save_path is not None:
-            print(secondary_save_path + save_path + '.png')
             plt.savefig(secondary_save_path + save_path + '.png')
 
-    return (files, std_files)
+    return (files, std_files, cleaned_names)
+
+
+def unpack_single_voltage(data: ([df.DataFrame], [df.DataFrame], [str]),
+                          array: [[float]], voltage: float) -> None:
+    files, std_files, names = data
+
+    for i, name in enumerate(names):
+        row: [] = [None for _ in columns]
+
+        row[0] = voltage
+
+        if 'KCL 1 X 10-4 M' in name:
+            row[1] = 'KCL 1 X 10-4 M'
+        elif 'KCL 5 X 10-5 M' in name:
+            row[1] = 'KCL 5 X 10-5 M'
+
+        if '5 micron' in name:
+            row[2] = 5.0
+        elif '10 micron' in name:
+            row[2] = 10.0
+
+        # Iterate over frequencies herein
+        data_rows = [item[1] for item in files[i].iterrows()]
+        std_rows = [item[1] for item in std_files[i].iterrows()]
+
+        for row_index in range(len(data_rows)):
+            row_copy = [item for item in row]
+
+            data_row = data_rows[row_index]
+            std_row = std_rows[row_index]
+
+            # Set frequency
+            row_copy[3] = data_row[0]
+
+            # Set mean sls
+            row_copy[4] = data_row['MEAN_STRAIGHT_LINE_SPEED']
+
+            # Set mean sls std
+            row_copy[5] = std_row['MEAN_STRAIGHT_LINE_SPEED_STD']
+
+            # print(row)
+            array.append(row_copy)
+
+    return
 
 
 if __name__ == '__main__':
-    os.chdir('/home/jorb/data_graphs/')
-    print(os.getcwd())
+    os.chdir(dir)
+    print('Working in directory', os.getcwd())
 
     # Voltage independent transition frequencies
     '''
@@ -138,6 +194,7 @@ if __name__ == '__main__':
     They must be in hertz.
     '''
 
+    '''
     # Real values:
     voltage_independent_transitions: [str] = [
         14500,
@@ -145,53 +202,37 @@ if __name__ == '__main__':
         10500,
         5500
     ]
-
     '''
+
     # Filler values:
     voltage_independent_transitions: [str] = ['10000.0' for _ in range(4)]
-    '''
 
-    data_5v: ([df.DataFrame], [df.DataFrame]) = load_voltage('(?<!1)5[_ ]?[Vv].*\\.csv', '5v',
-                                                             '/home/jorb/Programs/physicsScripts/',
-                                                             5.0,
-                                                             transitions=voltage_independent_transitions)
-    data_10v: ([df.DataFrame], [df.DataFrame]) = load_voltage('10[_ ]?[Vv].*\\.csv', '10v',
-                                                              '/home/jorb/Programs/physicsScripts/',
-                                                              10.0,
-                                                              transitions=voltage_independent_transitions)
-    data_15v: ([df.DataFrame], [df.DataFrame]) = load_voltage('15[_ ]?[Vv].*\\.csv', '15v',
-                                                              '/home/jorb/Programs/physicsScripts/',
-                                                              15.0,
-                                                              transitions=voltage_independent_transitions)
+    data_5v: ([df.DataFrame], [df.DataFrame], [str]) = load_voltage('(?<!1)5[_ ]?[Vv].*\\.csv', '5v',
+                                                                    '/home/jorb/Programs/physicsScripts/',
+                                                                    5.0,
+                                                                    transitions=voltage_independent_transitions)
+    data_10v: ([df.DataFrame], [df.DataFrame], [str]) = load_voltage('10[_ ]?[Vv].*\\.csv', '10v',
+                                                                     '/home/jorb/Programs/physicsScripts/',
+                                                                     10.0,
+                                                                     transitions=voltage_independent_transitions)
+    data_15v: ([df.DataFrame], [df.DataFrame], [str]) = load_voltage('15[_ ]?[Vv].*\\.csv', '15v',
+                                                                     '/home/jorb/Programs/physicsScripts/',
+                                                                     15.0,
+                                                                     transitions=voltage_independent_transitions)
 
-    exit(0)
+    array: [[float]] = []
 
-    # Make table
+    # Do 5v stuff
+    unpack_single_voltage(data_5v, array, 5.0)
 
-    '''
-    Output table structure:
+    # Do 10v stuff
+    unpack_single_voltage(data_10v, array, 10.0)
 
-    rows are frequency applied
-
-    columns are [voltage, solution, particle size, mean straight line speed]
-    '''
-
-    columns: [str] = ['VOLTAGE', 'SOLUTION',
-                      'SIZE_MICRONS', 'STRAIGHT_LINE_SPEED_PIXELS']
-
-    frequencies: [str] = ['0.0', '800.0', '1000.0', '10000.0', '25000.0',
-                          '50000.0', '75000.0', '100000.0', '150000.0', '200000.0', '300000.0']
-
-    array: [[float]] = [
-        [0.0 for _ in range(len(columns))] for _ in range(len(frequencies))]
-
-    for i, file in enumerate(files):
-        pass
+    # Do 15v stuff
+    unpack_single_voltage(data_15v, array, 15.0)
 
     df_array: df.DataFrame = df.DataFrame(array,
-                                          columns=columns,
-                                          index=frequencies)
-    df_array.to_csv(save_path + '.csv')
+                                          columns=columns)
+    df_array.to_csv(save_location)
 
-    if secondary_save_path is not None:
-        df_array.to_csv(save_path + '.csv')
+    exit(0)
